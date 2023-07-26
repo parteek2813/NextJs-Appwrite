38a517b678a6d9afd793189391bdfba907c4dffd
parteek kumar <digitalparteek@gmail.com>
2023-07-26 19:13:15 +0530

    added config for appwrite

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -1,6 +1,62 @@
 import conf from "@/conf/config";
 import { Client, Account, ID } from "appwrite";
 
-type CreateUserAccount = {};
+type CreateUserAccount = {
+  email: string;
+  password: string;
+  name: string;
+};
 
-type LoginUserAccount = {};
+type LoginUserAccount = {
+  email: string;
+  password: string;
+};
+
+const appwriteClient = new Client();
+
+appwriteClient.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+
+export const account = new Account(appwriteClient);
+
+export class AppwriteService {
+  // create a new record of user inside appwrite
+  async createUserAccount({ email, password, name }: CreateUserAccount) {
+    try {
+      const userAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      //   if useraccoutn exists, immediately login the user
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount; // else just return the userAccount
+      }
+    } catch (error: any) {
+      throw error;
+      console.log(error);
+    }
+  }
+
+  async login({ email, password }: LoginUserAccount) {
+    try {
+      return await account.createEmailSession(email, password);
+    } catch (error: any) {
+      throw error;
+    }
+  }
+
+  async isLoggedIn(): Promise<boolean> {
+    try {
+      const data = await this.getCurrentUser();
+    } catch (error: any) {}
+    return false;
+  }
+
+  async getCurrentUser() {}
+
+  async logout() {}
+}
