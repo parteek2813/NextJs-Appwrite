9f10abef467f4a5d853058db9cd26654c67bf414
parteek kumar <digitalparteek@gmail.com>
2023-07-27 10:55:04 +0530

    added custom layout and home page

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import appwriteService from "@/appwrite/config";
+import Blog from "@/components/Blog";
+import Header from "@/components/Header";
+import { AuthProvider } from "@/context/authContext";
+import React, { useState, useEffect } from "react";
+
+const ProtectedLayout = ({ children }: React.ReactNode) => {
+  const [authStatus, setAuthStatus] = useState(false);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    appwriteService
+      .isLoggedIn()
+      .then(setAuthStatus)
+      .finally(() => setLoader(false));
+  }, []);
+};
---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import useAuth from "@/context/useAuth";
+import React from "react";
+import ProfileCard from "@/components/ProfileCard";
+import Login from "@/components/Login";
+
+const Home = () => {
+  const { authStatus } = useAuth();
+  return <h1>Home Page</h1>;
+};
+
+export default Home;
